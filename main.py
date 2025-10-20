c71383dbb211943c3e0bd5d799872921ab2bb997
Quinton Khuwiseb <khuwisebq@gmail.com>
2025-10-20 19:12:20 +0200

    updated prompt to if

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ print("         🧙 Welcome to Cypherax 🧙")
 print("")
 print("##########################################") 
 print("")
-choice1 = int(input("State thy desire, my lord \n 1: to Encrypt the runes \n 2: to Decrypt their secrets? "))
-
-
-plaintext = input("Whisper thy sacred words, that I may seal them within the cryptic runes: ")
+choice1 = int(input("State thy desire, my lord \n 1: to Encrypt the runes \n 2: to Decrypt their secrets? \n"))
+if choice1 == 1:
+    plaintext = input("Whisper thy sacred words, that I may seal them within the cryptic runes: \n")
+else:
+     ciphertext = input("Whisper the runes of secrecy, that I may decipher their hidden wisdom, my lord: \n")
+  
