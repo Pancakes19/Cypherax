e5ad0143e59f59f2ad174cfcdb6490eed613c8a9
Quinton Khuwiseb <khuwisebq@gmail.com>
2025-10-20 18:49:27 +0200

    prompt to encrypt or decrypt

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 print("##########################################")
 print("")
-print("         Welcome to Cypherax💀")
+print("         🧙 Welcome to Cypherax 🧙")
 print("")
 print("##########################################") 
+print("")
+choice1 = int(input("State thy desire, my lord \n 1: to Encrypt the runes \n 2: to Decrypt their secrets? "))
+
+
+plaintext = input("Whisper thy sacred words, that I may seal them within the cryptic runes: ")
